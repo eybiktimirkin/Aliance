a99a26647ad589625ce2e764b91d66c3831d2112
Evgenii Biktimirkin <biktizhenya997@mail.ru>
2025-03-30 00:44:30 +0400

    fix: index.php

--- index.php
+++ index.php
@@ -762,7 +762,7 @@
         <div class="footer-wrapper">
           <div class="footer-legal">
             <p class="footer-copyright">
-              &copy; 2022 «Aliance Production». Все права защищены.
+              &copy; <?php echo date('d.m.y')?> «Aliance Production». Все права защищены.
             </p>
             <a href="#" class="footer-policy">Политики конфиденциальности</a>
           </div>
