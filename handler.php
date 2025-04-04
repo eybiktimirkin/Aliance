7318a372b85351a3fd855f785c5c7c74e62af8ac
Evgenii Biktimirkin <biktizhenya997@mail.ru>
2025-04-04 09:25:08 +0400

    add:send forms to telegrambot

--- handler.php
+++ handler.php
@@ -1,7 +1,24 @@
 <?php
 
-$user_name = $_POST["username"];
-$user_phone = $_POST["userphone"];
+$user_name = htmlspecialchars($_POST["username"]);
+$user_phone = htmlspecialchars($_POST["userphone"]);
 
-echo "Привет, " .$user_name . "<br>";
-echo "Ваш телефон: <b>" .$user_phone . "</b>";
+$token = "8061504938:AAE4l7u6hkXJysgg7Ka1L6F7MtdutCxdhuw";
+$chat_id = "-4691831124";
+
+$formData = array (
+  "Клиент: " => $user_name,
+  "Телефон: " => $user_phone
+);
+
+foreach($formData as $key => $value) {
+  $text .= $key . "<b>" . urlencode($value) . "</b>" . "%0A";
+}
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$text}&parse_mode=html", "r");
+
+if($sendToTelegram) {
+  echo "Success";
+} else {
+  echo "Error";
+}
