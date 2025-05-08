87b774c9572eaba4677c93fd364e6279366e973a
Evgenii Biktimirkin <biktizhenya997@mail.ru>
2025-05-08 13:31:19 +0400

    add:autohim-AG-page

--- autohim-ag.php
+++ autohim-ag.php
@@ -3,4 +3,79 @@ $page_title = "Автохимия AG-TECH";
 $header_style = "page-header-bg-grey";
 include_once('header-page.php'); ?>
 
-<?php include_once('footer.php');?>
+<section class="section autohim-ag-section">
+  <div class="container">
+    <div class="autohim-ag-content-wrapper">
+      <img src="img/avtohim/autohim-ag.jpg" alt="autohim-ag" class="autohim-photo-ag" />
+      <div class="autohim-ag-content">
+        <div class="seporator"></div>
+        <h2 class="section-title autohim-ag-title">Собственное производство автохимия AG-Tech</h2>
+        <p class="autohim-ag-text">
+          Прежде всего, начало повседневной работы по формированию позиции однозначно фиксирует необходимость
+          приоритизации разума над эмоциями. Кстати, представители современных социальных резервов неоднозначны и будут
+          описаны максимально подробно.
+          <br>
+          <br>
+          Но социально-экономическое развитие напрямую зависит от поставленных обществом задач! Безусловно, постоянный
+          количественный рост и сфера нашей активности предоставляет широкие возможности для системы обучения кадров,
+          соответствующей насущным потребностям. В целом, конечно, выбранный нами инновационный путь обеспечивает
+          широкому кругу (специалистов) участие в формировании своевременного выполнения сверхзадачи. А ещё элементы
+          политического процесса представлены в исключительно положительном свете. Высокий уровень вовлечения
+          представителей целевой аудитории является четким доказательством простого факта: высокотехнологичная концепция
+          общественного уклада создаёт предпосылки для глубокомысленных рассуждений. В своём стремлении повысить
+          качество жизни, они забывают, что дальнейшее развитие различных форм деятельности, а также свежий взгляд на
+          привычные вещи - безусловно открывает новые горизонты для поставленных обществом задач.
+        </p>
+      </div>
+    </div>
+    <?php include_once('template-parts/features-block.php') ?>
+  </div>
+</section>
+<section class="section section-catalog">
+  <div class="bg-grey catalog-wrapper">
+    <img src="./img/avtohim/book.jpg" alt="book" class="catalog-image" />
+    <div class="catalog-form container">
+      <div class="seporator"></div>
+      <h2 class="section-title avtohim-prod-title">Генеральный дистрибьютор премиальной
+        автохимии</h2>
+      <p class="avtohim-prod-text">
+        Внезапно, активно развивающиеся страны третьего мира могут быть объявлены нарушающими общечеловеческие нормы
+        этики и морали.
+      </p>
+      <ul class="avtohim-prod-list">
+        <li class="avtohim-prod-item">
+          <svg width="24" height="24" class="icon avtohim-prod-list-icon">
+            <use href="./img/sprite.svg#him"></use>
+          </svg>
+          Полное описание товаров
+        </li>
+        <li class="avtohim-prod-item">
+          <svg width="24" height="24" class="icon avtohim-prod-list-icon">
+            <use href="./img/sprite.svg#car"></use>
+          </svg>
+          Актуальные цены
+        </li>
+        <li class="avtohim-prod-item">
+          <svg width="24" height="24" class="icon avtohim-prod-list-icon">
+            <use href="./img/sprite.svg#brush"></use>
+          </svg>
+          Лаки и краски
+        </li>
+        <li class="avtohim-prod-item">
+          <svg width="24" height="24" class="icon avtohim-prod-list-icon">
+            <use href="./img/sprite.svg#him"></use>
+          </svg>
+          Химические производства
+        </li>
+      </ul>
+      <button type="submit" class="button catalog-button">
+        Скачать каталог
+      </button>
+    </div>
+  </div>
+</section>
+<?php include_once('template-parts/research-block.php') ?>
+<?php $block_title = "схема работы";
+include_once("./template-parts/steps-block.php") ?>
+<?php include_once("./template-parts/clients-block.php") ?>
+<?php include_once('footer.php'); ?>
--- template-parts/trademarks-block.php
+++ template-parts/trademarks-block.php
@@ -1,34 +1,34 @@
 <section class="section trademark section-trademark">
-      <div class="container">
-        <?php 
-        if (!empty($block_title)) {
-          echo '<div class="seporator"></div>';
-          echo '<h2 class="section-title">'. $block_title .'</h2>';
-        }
-        ?>
-        <div class="trademark-cards">
-          <div class="trademark-card">
-            <svg class="trademark-card-logo">
-              <use href="img/sprite.svg#AGTECH"></use>
-            </svg>
-            <h3 class="trademark-card-title">Автохимия AG-Tech</h3>
-            <p class="trademark-card-text">
-              Для современного мира разбавленное изрядной долей эмпатии,
-              рациональное мышление создаёт предпосылки для поставленных
-              обществом задач.
-            </p>
-          </div>
-          <div class="trademark-card">
-            <svg class="trademark-card-logo">
-              <use href="img/sprite.svg#AP"></use>
-            </svg>
-            <h3 class="trademark-card-title">Автохимия AP</h3>
-            <p class="trademark-card-text">
-              Для современного мира разбавленное изрядной долей эмпатии,
-              рациональное мышление создаёт предпосылки для поставленных
-              обществом задач.
-            </p>
-          </div>
-        </div>
+  <div class="container">
+    <?php
+    if (!empty($block_title)) {
+      echo '<div class="seporator"></div>';
+      echo '<h2 class="section-title">' . $block_title . '</h2>';
+    }
+    ?>
+    <div class="trademark-cards">
+      <a href="./autohim-ag.php" class="trademark-card">
+        <svg class="trademark-card-logo">
+          <use href="img/sprite.svg#AGTECH"></use>
+        </svg>
+        <h3 class="trademark-card-title">Автохимия AG-Tech</h3>
+        <p class="trademark-card-text">
+          Для современного мира разбавленное изрядной долей эмпатии,
+          рациональное мышление создаёт предпосылки для поставленных
+          обществом задач.
+        </p>
+      </a>
+      <div class="trademark-card">
+        <svg class="trademark-card-logo">
+          <use href="img/sprite.svg#AP"></use>
+        </svg>
+        <h3 class="trademark-card-title">Автохимия AP</h3>
+        <p class="trademark-card-text">
+          Для современного мира разбавленное изрядной долей эмпатии,
+          рациональное мышление создаёт предпосылки для поставленных
+          обществом задач.
+        </p>
       </div>
-    </section>
+    </div>
+  </div>
+</section>
